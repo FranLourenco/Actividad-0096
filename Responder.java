4db67132e96856b2b6e5783003aa293d42921d7c
Francisco <kiko_drosa@hotmail.com>
2014-03-24 01:54:20 +0100

    he añadido 5 respuestas diferentes que se mostraran de forma aleatoria al usuario

--- Responder.java
+++ Responder.java
@@ -1,3 +1,6 @@
+import java.util.Random;
+import java.util.ArrayList;
+
 /**
  * The responder class represents a response generator object.
  * It is used to generate an automatic response to an input string.
@@ -7,11 +10,21 @@
  */
 public class Responder
 {
+    Random aleatorio;
+    ArrayList <String> respuestas;
     /**
      * Construct a Responder - nothing to do
      */
     public Responder()
     {
+        aleatorio = new Random();
+        respuestas = new ArrayList<>();
+        
+        respuestas.add("El equipo no funciona");
+        respuestas.add("Pruebe a explicarme el problema de nuevo");
+        respuestas.add("En estos momentos no podemos solucionar el problema, pruebe más tarde");
+        respuestas.add("No conseguimos identificar el error que esta ocurriendo");
+        respuestas.add("El problema ya está solucionado");
     }
 
     /**
@@ -20,6 +33,7 @@ public class Responder
      */
     public String generateResponse()
     {
-        return "That sounds interesting. Tell me more...";
+        int numeroAleatorio = aleatorio.nextInt(respuestas.size());
+        return respuestas.get(numeroAleatorio);
     }
 }
